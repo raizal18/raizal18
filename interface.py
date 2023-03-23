8fc342ab1e50451ebcbe1386fd8c6a4d1d466ca4
raizal18 <ramakrishnanmurugan6@gmail.com>
2023-03-23 13:55:24 +0530

    machinlenious

--- interface.py
+++ interface.py
@@ -1,11 +1,7 @@
 import requests
 import requests_oauthlib
 from requests.exceptions import ConnectionError
-# try:
-#    r = requests.get("http://example.com", timeout=0.001)
-# except ConnectionError as e:    # This is the correct syntax
-#    print e
-#    r = "No response"
+
 ac = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJyYXN1IiwiZXhwIjoxNjc5NTU1MTk2fQ.Ql2cfpiR6qSKHadGMOzugydCXuDxDTsOISUyVnc4JyU"
 
 endpoint_url = "http://127.0.0.1:8000/blockchain/"
@@ -21,4 +17,4 @@ try:
 
 except ConnectionError as e:
 
-    print(e)
+    print("Server Not Running")
