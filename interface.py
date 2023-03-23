c3830a925213d3286d0a6cec9f49adc46db9daf9
raizal18 <ramakrishnanmurugan6@gmail.com>
2023-03-23 13:52:46 +0530

    req catch

--- interface.py
+++ interface.py
@@ -1,6 +1,11 @@
 import requests
 import requests_oauthlib
-
+from requests.exceptions import ConnectionError
+# try:
+#    r = requests.get("http://example.com", timeout=0.001)
+# except ConnectionError as e:    # This is the correct syntax
+#    print e
+#    r = "No response"
 ac = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJyYXN1IiwiZXhwIjoxNjc5NTU1MTk2fQ.Ql2cfpiR6qSKHadGMOzugydCXuDxDTsOISUyVnc4JyU"
 
 endpoint_url = "http://127.0.0.1:8000/blockchain/"
@@ -8,7 +13,12 @@ endpoint_url = "http://127.0.0.1:8000/blockchain/"
 headers = {
     "Authorization": f"Bearer {ac}"
 }
-response = requests.get(endpoint_url, headers=headers)
 
-# Print the response data
-print(response.json())
+try:
+
+    response = requests.get(endpoint_url, headers=headers)
+    print(response.json())
+
+except ConnectionError as e:
+
+    print(e)
