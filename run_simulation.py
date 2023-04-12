6310b2a9e0852dcb15a1711b3c241bf2ad365b8d
raizal18 <ramakrishnanmurugan6@gmail.com>
2023-04-12 17:18:29 +0530

    simulater run function

--- run_simulation.py
+++ run_simulation.py
@@ -3,4 +3,12 @@ import sys
 import math
 import json
 import numpy as np
+## RUN simulator_config.py in Different terrminal before start Simulation
+from intiate_simpy import run_simulator
 
+
+(CumThroughPut, 
+ThrouputPerNode,  
+userLatencyShard,  
+userlatencyNode, 
+NUMBEROFNODES, NUMBEROFSHARD) = run_simulator()
